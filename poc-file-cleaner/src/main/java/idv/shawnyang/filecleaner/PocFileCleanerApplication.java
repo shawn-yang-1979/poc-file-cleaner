e351dd2e6c05702e5b69723bfa4cf33232a6eb8a
SHAWN.SH.YANG <SHAWN.SH.YANG@TWTP1NB1024.delta.corp>
2019-03-05 15:14:23 +0800

    init

---
+++ poc-file-cleaner/src/main/java/idv/shawnyang/filecleaner/PocFileCleanerApplication.java
@@ -0,0 +1,13 @@
+package idv.shawnyang.filecleaner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocFileCleanerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocFileCleanerApplication.class, args);
+	}
+
+}
---
+++ poc-file-cleaner/src/test/java/idv/shawnyang/filecleaner/PocFileCleanerApplicationTests.java
@@ -0,0 +1,16 @@
+package idv.shawnyang.filecleaner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PocFileCleanerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
